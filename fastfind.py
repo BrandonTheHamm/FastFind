e8a94a98e3a851b438a135a09b272572f64bf80b
Brandon Hamm <brandon.hamm@gmail.com>
2023-08-21 10:57:02 -0500

    rg always separates json results by unix line endings, so can't use os.linesep

--- fastfind.py
+++ fastfind.py
@@ -6,7 +6,6 @@ import threading
 import errno
 import json
 from .typing import List, Optional
-from .structs import FastFindResult, ResultLocation
 
 #------------------------------------------------------------------------------
 FASTFIND_PLUGIN_DIR = os.path.basename(os.path.dirname(os.path.realpath(__file__)))
@@ -43,6 +42,31 @@ def get_setting(key, default=None, view=None):
 			'"FastFindSublime_after_context":1,\n'
 			'"FastFindSublime_display_outline": true,'.format(key))
 			sublime.error_message(error_string)
+
+
+#------------------------------------------------------------------------------
+class FastFindResult:
+	def __init__(self, filename: str, line_number: int, startpos: int, matchlen: int):
+		self.filename = filename
+		self.line_number = line_number
+		self.start_char_index = startpos
+		self.match_length = matchlen
+
+	@staticmethod
+	def from_json(json_content):
+		start_pos = int(json_content['submatches'][0]['start'])
+		match_len = int(json_content['submatches'][0]['end']) - start_pos + 1
+		return FastFindResult(json_content['path']['text'],
+			int(json_content['line_number']),
+			start_pos,
+			match_len)
+
+	def to_string(self) -> str:
+		return "FastFindResult: filename = {0}\n\tline_number = {1}\n\tstart_char_index = {2}\n\tmatch_length = {3}\n".format(
+			self.filename,
+			self.line_number,
+			self.start_char_index,
+			self.match_length)
 
 
 #------------------------------------------------------------------------------
@@ -88,7 +112,7 @@ class FastFindSublimeWorker(threading.Thread):
 			return None, None
 
 		cwd = os.path.dirname(current_filename)
-		print("FastFind: Current File Directory = {0}".format(cwd))
+		# print("FastFind: Current File Directory = {0}".format(cwd))
 		path = cwd
 
 		# check to see if a project is currently loaded
@@ -96,9 +120,9 @@ class FastFindSublimeWorker(threading.Thread):
 		if project_filename is not None and project_filename != "":
 			# use this project's directory as the base search path
 			path = os.path.dirname(self._view.window().project_file_name())
-			print("FastFind: Using project directory: {0}".format(path))
-		else:
-			print("FastFind: No project currently loaded")
+		# 	print("FastFind: Using project directory: {0}".format(path))
+		# else:
+		# 	print("FastFind: No project currently loaded")
 
 		if folder != "":
 			path = os.path.join(path, folder)
@@ -109,7 +133,7 @@ class FastFindSublimeWorker(threading.Thread):
 		fastfind_arg_list.append(word)
 		fastfind_arg_list.append(path)
 
-		print("FastFind: make_fastfind_cmd: fastfind_arg_list = {0}".format(fastfind_arg_list))
+		# print("FastFind: make_fastfind_cmd: fastfind_arg_list = {0}".format(fastfind_arg_list))
 		popen_arg_list = {
 			"shell": False,
 			"stdout": subprocess.PIPE,
@@ -136,7 +160,7 @@ class FastFindSublimeWorker(threading.Thread):
 
 		output, erroroutput = proc.communicate()
 
-		if erroroutput is not None and erroroutput != "":
+		if erroroutput is not None and erroroutput.strip() != "":
 			print("FastFind: erroroutput = '{0}'".format(erroroutput))
 
 		try:
@@ -144,7 +168,7 @@ class FastFindSublimeWorker(threading.Thread):
 		except TypeError:
 			output = unicode(str(output), encoding="utf8")
 
-		print("FastFind: output = {0}".format(output))
+		# print("FastFind: output = {0}".format(output))
 		return output
 
 	def process_results(self, results):
@@ -159,7 +183,9 @@ class FastFindSublimeWorker(threading.Thread):
 #------------------------------------------------------------------------------
 def parse_search_results_from_json(content: str) -> List[FastFindResult]:
 	results = []
-	for line in content.split(os.linesep):
+	# NOTE(BH): Ripgrep splits json results using Unix line-endings, so even on Windows,
+	# we need split the results on the Unix line termination character and not use os.linesep
+	for line in content.split("\n"):
 		# print("\n\nline:\n\n{0}\n\n".format(line))
 		line = line.strip()
 		if line == "":
@@ -201,7 +227,6 @@ class FastFindCommand(sublime_plugin.TextCommand):
 					dir = -1
 				elif count == 0:
 					dir = 1
-				print("FastFinding '%s' [%s=%s]" %(msgStr, ' ' * count, ' ' * (7 - count)))
 				sublime.set_timeout(lambda: self._update_status(workers, msgStr, show_results, count, dir), 100)
 				break
 
@@ -212,23 +237,22 @@ class FastFindCommand(sublime_plugin.TextCommand):
 				for worker in workers:
 					for result in worker._output:
 						self._find_results.append(result)
-					# self._display_results_scratch_window(worker.symbol, worker.output)
 					self._display_results_in_jump_list(worker._symbol, worker._output)
 
 	def _select_entry(self, index: int) -> None:
 		# print("_select_entry called with index = {0}".format(index))
 		if index < 0:
 			# return
-			print("cancelled navigation - return to previous position")
+			print("FastFind: cancelled navigation - return to previous position")
 			# cancelled, return to saved position
 			self.view.window().focus_view(self.view)
 			self.view.sel().clear()
 			self.view.sel().add(self._current_position)
 			self.view.set_viewport_position(self._saved_viewport_pos, animate=True)
 		else:
-			print("skipping...for index = {0}".format(index))
+			# print("skipping...for index = {0}".format(index))
 			selected_entry = self._find_results[index]
-			print("_select_entry %d: %s:%d:%d" % (index, selected_entry.filename, selected_entry.line_number, selected_entry.start_char_index))
+			# print("_select_entry %d: %s:%d:%d" % (index, selected_entry.filename, selected_entry.line_number, selected_entry.start_char_index))
 			view = self._open_basic_file(selected_entry.filename,
 				selected_entry.line_number,
 				selected_entry.start_char_index,
@@ -237,9 +261,9 @@ class FastFindCommand(sublime_plugin.TextCommand):
 		
 
 	def _highlight_entry(self, index: int) -> None:
-		print("_highlight_entry called with index = {0}".format(index))
+		# print("_highlight_entry called with index = {0}".format(index))
 		highlighted_result = self._find_results[index]
-		print("_highlight_entry %d : %s:%d:%d" % (index, highlighted_result.filename, highlighted_result.line_number, highlighted_result.start_char_index))
+		# print("_highlight_entry %d : %s:%d:%d" % (index, highlighted_result.filename, highlighted_result.line_number, highlighted_result.start_char_index))
 		preview = self._open_basic_file(highlighted_result.filename, 
 			highlighted_result.line_number,
 			highlighted_result.start_char_index,
@@ -259,7 +283,7 @@ class FastFindCommand(sublime_plugin.TextCommand):
 				"{0}:{1}".format(os.path.basename(location.filename), location.line_number),
 				annotation="FastFind"))
 
-		print("_display_results: num items = {0}".format(len(items)))
+		# print("_display_results: num items = {0}".format(len(items)))
 		window.show_quick_panel(items=items,
 			on_select=self._select_entry,
 			on_highlight=self._highlight_entry, 
